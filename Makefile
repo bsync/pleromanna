c18adc935fc18c63e026b266e583322bebc73c1a
Travis Horine <james.horine@gmail.com>
2018-11-11 09:08:41 -0600

    added basic upcoming event logic

--- Makefile
+++ Makefile
@@ -1,3 +1,8 @@
+ldev: .penv
+	bash -c 'export RDS_HOSTNAME=localhost RDS_USERNAME=travis && source .penv/bin/activate && cd pleromanna && python3 manage.py makemigrations'
+	bash -c 'export RDS_HOSTNAME=localhost RDS_USERNAME=travis && source .penv/bin/activate && cd pleromanna && python3 manage.py migrate'
+	bash -c 'export RDS_HOSTNAME=localhost RDS_USERNAME=travis && source .penv/bin/activate && cd pleromanna && python3 manage.py runserver 0.0.0.0:8000'
+
 dev: .penv
 	bash -c 'source .penv/bin/activate && cd pleromanna && python3 manage.py runserver 0.0.0.0:8000'
 
@@ -16,6 +21,7 @@ dbshell:
 	bash -c 'source .penv/bin/activate && cd pleromanna && python3 manage.py dbshell'
 
 zappadev:
+	bash -c 'source .penv/bin/activate && find . -name "*.pyc" -delete'
 	bash -c 'source .penv/bin/activate && cd pleromanna && zappa update dev'
 
 zaptail:
--- pleromanna/pleromanna/base/models.py
+++ pleromanna/pleromanna/base/models.py
@@ -2,27 +2,51 @@ from django.db import models
 
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel
-from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtailmenus.models import MenuPage
-
-class PleroPage(MenuPage):
-    image = models.ForeignKey(
-        'wagtailimages.Image',
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-        related_name='+'
-    )
-    paragraph = RichTextField(null=True)
-
-    content_panels = Page.content_panels + [
-        ImageChooserPanel('image'),
-        FieldPanel('paragraph', classname='full'), ]
+from wagtail.core.fields import StreamField
+from wagtail.admin.edit_handlers import StreamFieldPanel
+from wagtail.core import blocks as core_blocks
+from wagtail.images import blocks as image_blocks
+from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.admin.edit_handlers import FieldPanel
+from .blocks import PersonBlock, EventBlock
 
 
-class HomePage(PleroPage):
-    template = 'home_page.html'
+class PleromaHomePage(Page):
+    heading = models.CharField(max_length=256)
+    image = models.ForeignKey('wagtailimages.Image', null=True,
+                              on_delete=models.SET_NULL)
+    paragraph = RichTextField()
+    content_panels = Page.content_panels \
+                   + [  FieldPanel('heading'),
+                        ImageChooserPanel('image'),
+                        FieldPanel('paragraph'), ]
 
-class ContainerPage(Page):
-    pass
+    def get_context(self, request):
+        context = super(PleromaHomePage, self).get_context(request)
+        # Add extra variables and return the updated context
+        context['event_pages']=EventPage.objects.all().live()
+        return context
+
+
+class PleromaPage(MenuPage):
+    body = StreamField([
+        ('heading', core_blocks.CharBlock(classname="full title")),
+        ('subheading', core_blocks.CharBlock(classname="full title")),
+        ('paragraph', core_blocks.RichTextBlock()),
+        ('image', image_blocks.ImageChooserBlock()),
+        ('person', PersonBlock()),
+        ('html', core_blocks.RawHTMLBlock()),
+    ])
+
+    content_panels = MenuPage.content_panels + [
+        StreamFieldPanel('body'),
+    ]
+
+
+class EventPage(MenuPage):
+    events = StreamField([('event', EventBlock()), ], blank=True)
+
+    content_panels = MenuPage.content_panels + [
+        StreamFieldPanel('events'),
+    ]
