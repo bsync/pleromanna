a0f4f7dae353e23c6ac78aa1fbf4fcfdabe37ab4
dockerlight <james.horine@gmail.com>
2019-10-24 18:37:04 +0000

    Use docker secrets to configure gunicorn ssl.

--- gconfig.py
+++ gconfig.py
@@ -3,7 +3,6 @@ import multiprocessing
 reload = True
 bind = "0.0.0.0:8000"
 workers = multiprocessing.cpu_count() * 2 
-#workers = 1 
 accesslog = '-'
-certfile='/code/ssl/crt'
-keyfile='/code/ssl/key'
+certfile='/run/secrets/site.crt'
+keyfile='/run/secrets/site.key'
